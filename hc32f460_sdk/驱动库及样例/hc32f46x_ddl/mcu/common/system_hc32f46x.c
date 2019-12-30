2b5889636988d818bfed7db5c91fcb38d7fcfda9
skllii <hongjihuamail@163.com>
2019-12-30 17:31:03 +0800

    Set FPU function in system initialization stage.

--- hc32f460_sdk/驱动库及样例/hc32f46x_ddl/mcu/common/system_hc32f46x.c
+++ hc32f460_sdk/驱动库及样例/hc32f46x_ddl/mcu/common/system_hc32f46x.c
@@ -81,6 +81,10 @@ uint32_t SystemCoreClock = MRC_VALUE;
  ******************************************************************************/
 void SystemInit(void)
 {
+#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+    SCB->CPACR |= ((3UL << 20) | (3UL << 22)); /* set CP10 and CP11 Full Access */
+#endif
+
     SystemCoreClockUpdate();
 }
 
